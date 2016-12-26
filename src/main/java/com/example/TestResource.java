50df8092d4af3579538800c61d8807130bc217bc
snehalpansare <spansari@in.ibm.com>
2016-12-26 19:49:24 +0530

    4

--- src/main/java/com/example/TestResource.java
+++ src/main/java/com/example/TestResource.java
@@ -11,6 +11,8 @@ import javax.ws.rs.Produces;
 import com.ibm.json.java.JSON;
 import com.ibm.json.java.JSONArray;
 import com.ibm.json.java.JSONObject;
+import com.ibm.user.User;
+import com.ibm.user.UserDAO;
 
 
 // This class define the RESTful API to fetch the database service information
@@ -23,10 +25,21 @@ public class TestResource {
 	@Produces("application/json")
 	@Consumes("application/json")
 	public String getInformation(String input) throws Exception, IOException {
-        JSONObject myJSONObj = new JSONObject();
-        
-        myJSONObj.put("message", "Hello India!");
-        return myJSONObj.toString();
+        //JSONObject myJSONObj = new JSONObject();
+		String result = new String();
+		System.out.println("Snehal : " + input);
+        JSONObject json = new JSONObject().parse(input);
+		UserDAO userDao = new UserDAO();
+		User user = userDao.getUser((String) json.get("username"));
+		if (user != null)
+		{
+			if (user.getPassword().equals(json.get("password")))
+				result = "Login successful";
+		}
+		else
+			result = "Invalid Credentials";
+        //myJSONObj.put("message", "Hello India!");
+        return result;
         
 	}
 }
