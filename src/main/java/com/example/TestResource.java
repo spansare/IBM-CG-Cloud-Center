6ebbed651e3a73c0763ec71a560bb369beb6b615
snehalpansare <spansari@in.ibm.com>
2016-12-26 20:00:10 +0530

    5

--- src/main/java/com/example/TestResource.java
+++ src/main/java/com/example/TestResource.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -21,7 +22,7 @@ import com.ibm.user.UserDAO;
 @Path("/test")
 public class TestResource {
 
-	@GET
+	@POST
 	@Produces("application/json")
 	@Consumes("application/json")
 	public String getInformation(String input) throws Exception, IOException {
