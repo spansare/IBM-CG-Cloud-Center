5f0f0008ce2888562d815c5e63af05b7f1427fe4
snehalpansare <spansari@in.ibm.com>
2016-12-27 22:28:52 +0530

    16

--- src/main/webapp/controller/categoryController.js
+++ src/main/webapp/controller/categoryController.js
@@ -1,7 +1,5 @@
 dashboardApp.controller('categoryController', function($scope, $http) {
    
-	var urls = "/api/CategoryService/getCategories";
-	
 	
 	$http({
 		method : 'GET',
@@ -14,5 +12,24 @@ dashboardApp.controller('categoryController', function($scope, $http) {
 	});
 
 	
+	
+	$scope.user = {
+			username: "",
+			password: "",
+			
+			validateLogin: function() {
+				var jsonData = "{\"username\":" + $scope.user.username + ",\"password\":" + $scope.user.password + "}";
+				$http({
+					method : 'POST',
+					url : 'api/UserService/validateLogin',
+					data : jsonData
+				}).success(function(data, status, headers, config) {
+					$scope.loginResult = data;
+				}).error(function(data, status, headers, config) {
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
+				});
+			}
+	}
 
 });
