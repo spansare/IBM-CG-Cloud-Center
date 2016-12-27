8b2655d2f05c83547e356dc38bc04bf37a1d2698
snehalpansare <spansari@in.ibm.com>
2016-12-27 19:48:22 +0530

    13

--- src/main/webapp/controller/categoryController.js
+++ src/main/webapp/controller/categoryController.js
@@ -7,7 +7,7 @@ dashboardApp.controller('categoryController', function($scope, $http) {
 		method : 'GET',
 		url : 'api/CategoryService/getCategories'
 	}).success(function(data, status, headers, config) {
-		$scope.person = data;
+		$scope.categoryList = data.result;
 	}).error(function(data, status, headers, config) {
 		// called asynchronously if an error occurs
 		// or server returns response with an error status.
