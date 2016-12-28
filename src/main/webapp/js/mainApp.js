fece6b2fad5660657f183aadc816b2b324c569a0
snehalpansare <spansari@in.ibm.com>
2016-12-28 19:31:29 +0530

    1

---
+++ src/main/webapp/js/mainApp.js
@@ -0,0 +1,58 @@
+var mainApp = angular.module('mainApp', ['ngRoute']);
+
+
+mainApp.controller('categoryController', ['$rootScope', '$scope', 'assetService', function ($rootScope, $scope, assetService) {
+
+	$scope.assetService = assetService;   
+   
+	$http({
+		method : 'GET',
+	url : 'api/CategoryService/getCategories'
+	}).success(function(data, status, headers, config) {
+		$scope.categoryList = data.result;
+	}).error(function(data, status, headers, config) {
+		// called asynchronously if an error occurs
+	// or server returns response with an error status.
+	});
+	
+	$scope.getAssetsbyCategory = function(categoryName) {
+		
+		console.log(categoryName);
+		var url = "http://" + $window.location.host + "/asset.html";
+    	$window.location.href = url;
+	}
+       
+}]);
+
+
+
+mainApp.controller('assetController', ['$rootScope', '$scope', 'assetService', function ($rootScope, $scope, assetService) {
+
+	$scope.assetService = assetService;
+	
+	$scope.categoryName = "Live Demo with Bluemix";
+	
+	console.log($scope.categoryName);
+	$http({
+		method : 'POST',
+		url : 'api/AssetService/getAssets',
+		data : {
+			'category' : $scope.categoryName
+		}
+	}).success(function(data, status, headers, config) {
+		$scope.assetList = data.result;
+	}).error(function(data, status, headers, config) {
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
+	});
+
+    
+}]);
+
+
+    
+mainApp.service('assetService', function() {
+	
+      this.xxx = "yyy";
+    
+});
